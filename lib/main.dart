ee653a335a6ab4ad3ebc4ccc60200c2bc291498f
Harshit Rathod <harshitrathod@gmail.com>
2020-07-16 01:09:11 +0530

    final code

--- lib/main.dart
+++ lib/main.dart
@@ -9,69 +9,35 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  Expanded buildkey({Color color, int soundNumber}) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        enableFeedback: false,
+        onPressed: () {
+          playsound(soundNumber);
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              MaterialButton(
-                color: Colors.red,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(1);
-                },
-                child: null,
-              ),
-              MaterialButton(
-                color: Colors.yellow,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(2);
-                },
-                child: null,
-              ),
-              MaterialButton(
-                color: Colors.blue,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(3);
-                },
-                child: null,
-              ),
-              MaterialButton(
-                color: Colors.green,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(4);
-                },
-                child: null,
-              ),
-              MaterialButton(
-                color: Colors.orange,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(5);
-                },
-                child: null,
-              ),
-              MaterialButton(
-                color: Colors.purple,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(6);
-                },
-                child: null,
-              ),
-              MaterialButton(
-                color: Colors.lightGreen,
-                enableFeedback: false,
-                onPressed: () {
-                  playsound(7);
-                },
-                child: null,
-              ),
+              buildkey(color: Colors.red, soundNumber: 1),
+              buildkey(color: Colors.green, soundNumber: 2),
+              buildkey(color: Colors.purple, soundNumber: 3),
+              buildkey(color: Colors.orange, soundNumber: 4),
+              buildkey(color: Colors.yellow, soundNumber: 5),
+              buildkey(color: Colors.blue, soundNumber: 6),
+              buildkey(color: Colors.teal, soundNumber: 7),
             ],
           ),
         ),
